1a40e805aa66d74e6892d8143dac997682e60e16
ChiragM999 <106869092+ChiragM999@users.noreply.github.com>
2023-06-23 18:56:55 +0530

    Update config.js

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = `a83b439b-28dd-4a74-b6b3-63d4331ace92`;
+export const KEY = `0eabb98c-d05e-4f05-b6de-9079305b03af`;
 export const MODAL_CLOSE_SEC = 2.5;
